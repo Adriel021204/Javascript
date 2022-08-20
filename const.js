d31ff74202655a0497d25fc350c540d9ce74fca0
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-20 09:50:44 +0530

    edited bye adriel

--- const.js
+++ const.js
@@ -1,4 +1,8 @@
+//declaring @ initializing a constant
+const pi = 3.14
+console.log(pi);
+
+// const values do not change unlike var in js
 const interestRate = 0.5;
 interestRate = 1;
-
-console.log(interestRate);               // const values do not change unlike var in js
+console.log(interestRate);

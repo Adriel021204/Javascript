43104cd51d98ad25e19591a353b1ceb0ac3e2dc6
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-22 15:03:57 +0530

    edited by adriel

---
+++ Tutorial_1/var.js
@@ -0,0 +1,16 @@
+/*
+Reserverd keywords cannot be used as var names
+Var names given should be meaningful
+Cannot start with a number eg. 1Adriel
+Cannot contain spaces or a hyphen eg. Adr iel , Adr_iel
+Case-sensitive*/
+
+let firstName = 'Adriel';     //Camel convention
+let LastName = 'Fernandes';  //Pascal convention
+
+console.log(firstName,LastName);
+
+var firstN = 'Adriel';
+var LastN = 'Fernandes';
+
+console.log(firstN,LastN);

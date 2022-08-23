1ee0ad2e5c9b54cca72bf7abf60117e176ff1d65
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-23 13:10:19 +0530

    edited by adriel

---
+++ Tutorial_2/8_squen.js
@@ -0,0 +1,12 @@
+/**
+ *  \' single quote
+ *  \" double quote
+ *  \\ backslash
+ *  \n newline
+ *  \t tab
+ *  \b backspace
+ *  \f form feed
+ **/
+
+var myStr = "firstLine\n\t\\secondLine\n thirdLine";
+console.log(myStr);

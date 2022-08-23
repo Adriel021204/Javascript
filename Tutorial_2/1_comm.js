b4d9574727028567072f969378a404d281ae5af1
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-23 11:42:30 +0530

    edited by adriel

---
+++ Tutorial_2/1_comm.js
@@ -0,0 +1,6 @@
+//Always comment your code in js
+
+//Single_line_comment
+
+/*Multi_line
+ comment*/
---
+++ Tutorial_2/2_var.js
@@ -0,0 +1,11 @@
+/*
+Data ypes in js
+string,number,null,boolean,undefined,object,symbol
+*/
+
+var word = 'Beau';
+word = 4;
+
+let name = 'adriel';
+
+const pi = 3.14;

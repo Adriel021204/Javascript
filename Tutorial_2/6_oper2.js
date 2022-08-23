1136b6c0d91d22c10a9940364096515ec94b1e21
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-23 12:46:36 +0530

    edited by adriel

---
+++ Tutorial_2/6_oper2.js
@@ -0,0 +1,46 @@
+//addition//
+var a = 1;
+var b = 2;
+var c = 4;
+
+/*
+a = a + 2;
+b = b + 4;
+c = c + 8;
+*/
+
+a += 2;
+b += 4;
+c += 8;
+
+
+
+//multiplication/
+var a = 1;
+var b = 2;
+var c = 4;
+
+/*
+a = a * 2;
+b = b * 4;
+c = c * 8;
+*/
+
+a *= 2;
+b *= 4;
+c *= 8;
+
+//division//
+var a = 2;
+var b = 4;
+var c = 8;
+
+/*
+a = a / 1;
+b = b / 2;
+c = c / 4;
+*/
+
+a /= 1;
+b /= 2;
+c /= 4;
---
+++ Tutorial_2/7_str.js
@@ -0,0 +1,6 @@
+
+//var myStr = "I am a"double quoted"string inside  "double quotes"";
+
+//For not marking the end of a string use \ to indicate continuation of the string//
+var myStr = "I am a \"double quoted\" string inside \"double quotes";
+console.log(myStr);

932512e1a25ef5b2d9d9d0d717ac2a49d362b205
Adriel Fernandes <adrielferns021204@gmail.com>
2022-09-19 14:45:57 +0530

    edited by adriel

---
+++ Tutorial_2/17_equ.js
@@ -0,0 +1,8 @@
+//to check if value of val is equal to 12//
+function testEqual(val) {
+    if (val == 12) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+console.log(testEqual(10));  //change value to test
